192fb3d5e8e070e23706ac2c29c15d7cc2230881
Santosh Bhosale <santoshb@inboxops.local>
2023-03-29 03:50:55 +0530

    usingApi

--- src/components/News.js
+++ src/components/News.js
@@ -2,33 +2,25 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles= [{
-        "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-      },
-      {
-        "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-      }]
+    
     constructor(){
         super();
         this.state ={
-            articles: this.articles,
+            articles: [],
             loading:false
         }
        // console.log(this.state.articles);
+    }
+
+   async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3dd0f76c4e5542f88b0ed2c86e1b5dca";
+        let data =  await fetch(url);
+        //console.log(data);
+        let parseData = await data.json();
+        // console.log(parseData)
+
+        this.setState({articles:parseData.articles})
+
     }
   render() {
    
@@ -39,7 +31,7 @@ export class News extends Component {
      <div className="row"> 
      {this.state.articles.map((element)=>{
         return  <div className="col-md-4" key={element.url} >
-        <NewsItem  title={element.title.slice(0,45)} description = {element.description.slice(0,88)} imageUrl ={element.urlToImage} newsUrl ={element.url}></NewsItem>
+        <NewsItem  title={element.title?element.title.slice(0,45):""} description = {element.description?element.description.slice(0,88):""} imageUrl ={element.urlToImage} newsUrl ={element.url}></NewsItem>
         </div> 
      })}
     
